5260c2801dbaa12b1ea3077a0fa39fdee4a70cab
Surasak Chaisurin <surasak.c.work@gmail.com>
2021-11-23 18:04:36 +0700

    chore: apply custom theme to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        "ebony-clay": "#252836",
+        "burnt-sienna": "#EA7C69",
+        "tower-gray": "#ABBBC2",
+        "steel-gray": "#1F1D2B",
+        "black-lighter": "rgba(0, 0, 0, 0.7)",
+      },
+    },
   },
   variants: {
     extend: {},
