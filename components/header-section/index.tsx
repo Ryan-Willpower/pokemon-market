f326e46254ef42ca81aa23fdcaa85816b77c90b4
Surasak Chaisurin <surasak.c.work@gmail.com>
2021-11-24 21:43:21 +0700

    feat: add search bar component

--- components/header-section/index.tsx
+++ components/header-section/index.tsx
@@ -1,9 +1,11 @@
+import { SearchBar } from "../searchbar"
+
 export function HeaderSection() {
   return (
     <header className="flex justify-between py-8">
       <h1 className="text-2xl font-medium">Pokemon market</h1>
       <div>
-        <input placeholder="Search by Name" />
+        <SearchBar />
         <button>x</button>
       </div>
     </header>
---
+++ components/searchbar/index.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image"
+
+import searchIcon from "../../public/icons/search.png"
+
+export function SearchBar() {
+  return (
+    <div className="relative">
+      <div className="absolute inset-y-0 left-0 flex items-center pl-4">
+        <Image src={searchIcon} width={20} height={20} alt="search icon" />
+      </div>
+      <input
+        className="bg-transparent p-4 pl-12 border rounded border-solid outline-none border-white-darker focus:border-white focus:text-white"
+        placeholder="Search by Name"
+      />
+    </div>
+  )
+}
