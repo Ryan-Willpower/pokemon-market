c6bfb969c5fdf83fd98ade63e879ba3c23737a15
Surasak Chaisurin <surasak.c.work@gmail.com>
2021-11-25 15:45:59 +0700

    feat: add `add-to-cart-button` component

---
+++ components/add-to-cart-button/index.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react"
+import { AddToCartButton } from "."
+
+describe("<AddToCartButton />", () => {
+  it("should render without any crash", () => {
+    render(<AddToCartButton />)
+  })
+
+  it("should render search icon image", () => {
+    render(<AddToCartButton />)
+
+    expect(screen.getByRole("img")).toBeInTheDocument()
+  })
+
+  it("should render search icon image with alt message", () => {
+    render(<AddToCartButton />)
+
+    expect(screen.getByRole("img")).toHaveAttribute("alt", "shopping cart icon")
+  })
+
+  it("should disable the button when `isDisable` is pass to the properties", () => {
+    render(<AddToCartButton isDisable />)
+
+    expect(screen.getByText("Add to cart")).toBeDisabled()
+  })
+})
